705837cb230eb410f72c21b26b51d175ada75783
oktyajipangestu <oktyajipangestu@gmail.com>
2021-07-21 14:46:51 +0700

    integrasi bootstrap dan react js

--- src/Middleware.js
+++ src/Middleware.js
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import App from './App';
+import Login from './components/Login';
 
 const Middleware = () => {
     return (
         <Router>
             <Switch>
                 <Route exact path="/" render={() => <App /> }/>
+                <Route exact path="/login-admin" render={() => <Login />} />
             </Switch>
         </Router>
     )
---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+const Login = () => {
+    return(
+        <>
+            <button className="btn btn-primary">Save</button>
+        </>
+    );
+}
+
+export default Login;
